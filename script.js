bb6ebbf310b068696df37a14e0bb9c10192922a2
leonelp8111 <114585927+leonelp8111@users.noreply.github.com>
2022-09-28 08:49:57 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Leonelp"; // update the part BETWEEN the "quotes"
+let userName = "leonelp8111"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
